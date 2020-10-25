0a02decd33fc533b74280108b730d955e142264a
Wilson Birch <wilsonbirch@gmail.com>
2020-10-25 12:44:20 -0400

    updated server file

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 //Connect to the mongo db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Send every request to the React app
 // Define any API routes before this runs
