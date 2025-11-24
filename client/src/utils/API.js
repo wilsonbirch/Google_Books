fa5958ee0bd38db995d09b9a08d1c367db120170
wilbir <wilsonbirch@gmail.com>
2025-11-23 23:20:39 -0500

    updated google_books to work with apiKey hidden in .env vars

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,20 @@
-import axios from "axios";
+import axios from 'axios'
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
-};
+    // Gets all books
+    getBooks: function () {
+        return axios.get('/api/books')
+    },
+    // Deletes the book with the given id
+    deleteBook: function (id) {
+        return axios.delete('/api/books/' + id)
+    },
+    // Saves a book to the database
+    saveBook: function (bookData) {
+        return axios.post('/api/books', bookData)
+    },
+    // Searches Google Books API
+    searchBooks: function (query) {
+        return axios.get('/api/books/search?query=' + query)
+    },
+}
